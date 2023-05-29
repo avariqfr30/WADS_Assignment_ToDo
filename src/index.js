b686645f132714d8896fd12b42fbc2f42993bda8
Avariq Fazlur Rahman <91586313+avariqfr30@users.noreply.github.com>
2023-05-29 22:40:58 +0700

    yippeee

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const DATA = [
+const data = [
   { id: "todo-0", name: "Comp Phys FP", completed: true },
   { id: "todo-1", name: "WADS FP", completed: false },
   { id: "todo-2", name: "Indonesian FP", completed: false },
@@ -14,6 +14,6 @@ const DATA = [
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App assignments={DATA} />
+    <App assignments={data} />
   </React.StrictMode>
 );
